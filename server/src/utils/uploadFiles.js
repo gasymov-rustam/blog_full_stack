cbd27bc1bef7ed7a93af99d75f6ac0ae2ea38a48
Rustam Gasymov <rustam.gasymov@devalore.com>
2022-10-22 06:31:15 +0300

    create folder uploads if is not exist

--- server/src/utils/uploadFiles.js
+++ server/src/utils/uploadFiles.js
@@ -1,12 +1,16 @@
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
     cb(null, path.join(__dirname, '..', 'uploads'));
   },
   filename: (_, file, cb) => {
